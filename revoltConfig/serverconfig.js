a0940f00d8334cc3c71baf98c05d7d4c31b91a1e
vickyyadav4rattan <76946234+vickyyadav4rattan@users.noreply.github.com>
2021-03-02 12:18:09 +0530

    Update serverconfig.js

--- revoltConfig/serverconfig.js
+++ revoltConfig/serverconfig.js
@@ -26,7 +26,7 @@ const CONFIG = {
     },
     //Graphql Server Options (GQL9973)
     GraphQlserverOptions: {
-        port: $PORT||process.env.PORT,
+        port: `${PORT}`||process.env.PORT,
 //         endpoint: '/graphql',
 //         subscriptions: '/subscriptions',
 //         playground: "/playground",
